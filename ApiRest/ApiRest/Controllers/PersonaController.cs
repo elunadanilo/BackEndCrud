3e7da1f2fcc2e0d4dd1a6d0ba7bdacde80abe714
elunadanilo <eluna.danilo@gmail.com>
2021-08-07 00:38:06 -0600

    BackEnd Crud
    Primera version de backend

---
+++ ApiRest/ApiRest/Controllers/PersonaController.cs
@@ -0,0 +1,93 @@
+﻿using AccessData;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiRest.Controllers
+{
+    public class PersonaController : ApiController
+    {
+ 
+        private DbPersonaEntities personas = new DbPersonaEntities();
+
+        [HttpGet]
+        public IEnumerable<TblPersona> Get()
+        {
+            using (personas)
+
+            {
+                return personas.TblPersona.ToList();
+            }
+        }
+
+        [HttpGet]
+        public TblPersona Get(int id)
+        {
+            using (DbPersonaEntities personas = new DbPersonaEntities())
+
+            {
+                return personas.TblPersona.FirstOrDefault(e => e.IdPersona == id);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult AgregarPersona([FromBody] TblPersona persona) {
+            if (ModelState.IsValid)
+            {
+                personas.TblPersona.Add(persona);
+                personas.SaveChanges();
+                return Ok(persona);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(int id, [FromBody]TblPersona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                var ExistPersona = personas.TblPersona.Count(e => e.IdPersona == id) > 0;
+
+                if (ExistPersona)
+                {
+                    personas.Entry(persona).State = EntityState.Modified;
+                    personas.SaveChanges();
+
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
+            var persona = personas.TblPersona.Find(id);
+
+            if (persona != null)
+            {
+                personas.TblPersona.Remove(persona);
+                personas.SaveChanges();
+                return Ok(persona);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
